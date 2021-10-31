5b2d962564212fe27eda67503d97a5c4c3184a79
Matthewk99 <matthewkirby99@gmail.com>
2021-10-31 15:28:26 -0400

    OverlappingRectangles-Matthew

--- demos/src/main/java/demos/OverlappingRectangles.java
+++ demos/src/main/java/demos/OverlappingRectangles.java
@@ -4,6 +4,20 @@ import javax.sound.sampled.Line;
 
 import edu.wofford.woclo.*;
 
+// Total Area = (Area of 1st rectangle + 
+//              Area of 2nd rectangle) - 
+//              Area of Intersecting part
+// need to heck if there is indeed an overlap.
+
+public static class Point {
+        int x, y;
+ 
+        public Point(int x, int y) {
+          this.x = x;
+          this.y = y;
+        }
+};
+
 public class OverlappingRectangles {
 
   public static void main(String... args) {
@@ -16,8 +30,25 @@ public class OverlappingRectangles {
   }
 
   public OverlappingRectangles(Integer[] coord) {
+    int coord[] = new int[] { args }; 
     int totalArea = 0;
     int overlapArea = 0;
+
+    // bottom left point of the overlapping area.
+    // random parameter subs right now
+	  int bl_x = Math.max(x1, y1);
+	  int bl_y = Math.max(x3, y3);
+
+	  // top right point of the overlapping area.
+	  int tr_x = Math.min(x2, y2);
+	  int tr_y = Math.min(x4, y4);
+
+    // if overlapping need something like "if(right > left && top > bottom)" then
+    // OverlapArea should be the addition of area of the two rectangles minus the overlapping area.
+
+
+	
+	  totalArea = (tr_x - bl_x) * (tr_y - bl_y);
 
     System.out.println(totalArea + " " + overlapArea);
   }
