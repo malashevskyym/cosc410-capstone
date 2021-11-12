1b92ecd3c15df0b91a1baccbee777b0a9a523496
Allen McNorrill <apmcnorrill@gmail.com>
2021-11-12 18:15:47 -0500

    MaximalLayers foundation

--- demos/src/main/java/demos/MaximalLayers.java
+++ demos/src/main/java/demos/MaximalLayers.java
@@ -4,5 +4,27 @@ import edu.wofford.woclo.*;
 
 public class MaximalLayers {
 
-  public static void main(String... args) {}
+  public static void main(String... args) {
+    LineParser parser =
+        new LineParser(
+            "java MaximalLayers [-h] [--sortedX] [--sortedY] points",
+            "Sort the points into layers.");
+    parser.addRequiredArgument("points", LineParser.Datatype.STRING, "the first string");
+    parser.addRequiredArgument("sortedX", LineParser.Datatype.BOOLEAN, "sort layers by x coordinate");
+    parser.addRequiredArgument("sortedY", LineParser.Datatype.BOOLEAN, "sort layers by y coordinate");
+
+    try {
+      parser.parse(args);
+      MaximalLayers maximalLayers =
+          new MaximalLayers(parser.getArgument("points"));
+
+      //System.out.println(equivalentStrings.message);
+    } catch (Exception e) {
+      System.out.println("MaximalLayers error: " + e.getMessage());
+    }
+  }
+
+  public MaximalLayers() {
+    
+  }
 }
---
+++ demos/src/test/java/demos/MaximalLayersTest.java
@@ -0,0 +1,8 @@
+package demos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.*;
+import org.junit.jupiter.api.Test;
+
+class MaximalLayersTest {}
