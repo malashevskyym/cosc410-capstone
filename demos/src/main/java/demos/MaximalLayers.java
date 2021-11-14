b041e32e892aaea91abdf7e21a8059f4c5e5e7d3
allenmcnorrill <apmcnorrill@gmail.com>
2021-11-14 02:29:51 -0500

    MaximalLayers

--- demos/src/main/java/demos/MaximalLayers.java
+++ demos/src/main/java/demos/MaximalLayers.java
@@ -1,8 +1,12 @@
 package demos;
 
 import edu.wofford.woclo.*;
+import java.util.*;
 
 public class MaximalLayers {
+  Vector<int[]> points = new Vector<int[]>(15);
+  String layers = "";
+  String error = "";
 
   public static void main(String... args) {
     LineParser parser =
@@ -10,21 +14,44 @@ public class MaximalLayers {
             "java MaximalLayers [-h] [--sortedX] [--sortedY] points",
             "Sort the points into layers.");
     parser.addRequiredArgument("points", LineParser.Datatype.STRING, "the first string");
-    parser.addRequiredArgument("sortedX", LineParser.Datatype.BOOLEAN, "sort layers by x coordinate");
-    parser.addRequiredArgument("sortedY", LineParser.Datatype.BOOLEAN, "sort layers by y coordinate");
+    parser.addOptionalArgument(
+        "sortedX", LineParser.Datatype.BOOLEAN, "sort layers by x coordinate");
+    parser.addOptionalArgument(
+        "sortedY", LineParser.Datatype.BOOLEAN, "sort layers by y coordinate");
 
     try {
       parser.parse(args);
-      MaximalLayers maximalLayers =
-          new MaximalLayers(parser.getArgument("points"));
+      MaximalLayers maximalLayers = new MaximalLayers(parser.getArgument("points"));
+      if (parser.getArgument("sortedY")) {}
 
-      //System.out.println(equivalentStrings.message);
+      if (parser.getArgument("sortedX")) {}
+
+      maximalLayers.findLayers();
+
+      System.out.println(maximalLayers.layers);
     } catch (Exception e) {
       System.out.println("MaximalLayers error: " + e.getMessage());
     }
   }
 
-  public MaximalLayers() {
-    
+  public MaximalLayers(String points) {
+    String[] pointsArray = points.split(",");
+    /*
+    if (pointsArray.length % 2 != 0) {
+      throw new IllegalArgumentException(pointsArray[pointsArray.length - 1] + " is an unpaired x coordinate");
+    }
+    */
+    for (int i = 0; i < pointsArray.length; i += 2) {
+      this.points.addElement(
+          new int[] {Integer.valueOf(pointsArray[i]), Integer.parseInt(pointsArray[i + 1])});
+    }
+
+    layers = findLayers();
+  }
+
+  public String findLayers() {
+    String s = "";
+
+    return s;
   }
 }
--- demos/src/test/java/demos/MaximalLayersTest.java
+++ demos/src/test/java/demos/MaximalLayersTest.java
@@ -5,4 +5,76 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.*;
 import org.junit.jupiter.api.Test;
 
-class MaximalLayersTest {}
+class MaximalLayersTest {
+  @Test
+  public void testOneLayer() {
+    String[] testInput = new String[] {"5,5"};
+    MaximalLayers maxLayerTest = new MaximalLayers(testInput[0]);
+
+    assertEquals("1:(5,5)", maxLayerTest.layers);
+  }
+
+  @Test
+  public void testTwoLayers() {
+    String[] testInput = new String[] {"5,5,4,9,10,2"};
+    MaximalLayers maxLayerTest = new MaximalLayers(testInput[0]);
+
+    assertEquals("1:(4,9) 2:(5,5)(10,2)", maxLayerTest.layers);
+  }
+
+  @Test
+  public void testThreeLayers() {
+    String[] testInput = new String[] {"5,5,4,9,10,2,2,3,15,7"};
+    MaximalLayers maxLayerTest = new MaximalLayers(testInput[0]);
+
+    assertEquals("1:(4,9)(15,7) 2:(5,5)(10,2) 3:(2,3)", maxLayerTest.layers);
+  }
+
+  @Test
+  public void testLayerWithTwoMaximalPoints() {
+    String[] testInput = new String[] {"1,5,5,5,4,9,10,2,2,3,15,7"};
+    MaximalLayers maxLayerTest = new MaximalLayers(testInput[0]);
+
+    assertEquals("1:(4,9)(15,7) 2:(1,5)(5,5)(10,2) 3:(2,3)", maxLayerTest.layers);
+  }
+
+  @Test
+  public void testSortedX() {
+    String[] testInput = new String[] {"1,5,5,5,4,9,10,2,2,3,15,7", "--sortedX"};
+    MaximalLayers maxLayerTest = new MaximalLayers(testInput[0]);
+
+    assertEquals("1:(4,9)(15,7) 2:(1,5)(5,5)(10,2) 3:(2,3)", maxLayerTest.layers);
+  }
+
+  @Test
+  public void testSortedY() {
+    String[] testInput = new String[] {"1,5,5,5,4,9,10,2,2,3,15,7", "--sortedY"};
+    MaximalLayers maxLayerTest = new MaximalLayers(testInput[0]);
+
+    assertEquals("1:(4,9)(15,7) 2:(1,5)(5,5)(10,2) 3:(2,3)", maxLayerTest.layers);
+  }
+
+  @Test
+  public void testSortedXAndSortedY() {
+    String[] testInput = new String[] {"1,5,5,5,4,9,10,2,2,3,15,7", "--sortedX", "--sortedY"};
+    MaximalLayers maxLayerTest = new MaximalLayers(testInput[0]);
+
+    assertEquals("1:(4,9)(15,7) 2:(1,5)(5,5)(10,2) 3:(2,3)", maxLayerTest.layers);
+  }
+
+  @Test
+  public void testUnpairedXCoordinate() {
+    String[] testInput = new String[] {"5,5,4,9,10,2,2,3,15"};
+    MaximalLayers maxLayerTest = new MaximalLayers(testInput[0]);
+
+    assertEquals("MaximalLayers error: 15 is an unpaired x coordinate", maxLayerTest.error);
+  }
+
+  @Test
+  public void testNotOfTypeInteger() {
+    String[] testInput = new String[] {"5,5,4,9,10,x,2,3,15,7"};
+    MaximalLayers maxLayerTest = new MaximalLayers(testInput[0]);
+
+    assertEquals("MaximalLayers error: the value x is not of type integer", maxLayerTest.error);
+  }
+}
